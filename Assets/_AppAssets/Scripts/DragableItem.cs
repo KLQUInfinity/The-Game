b7f54f0040f8e778a05dbed6e8ce9f6a6d4220d0
KLQUInfinity <31450453+KLQUInfinity@users.noreply.github.com>
2020-04-03 13:30:05 +0200

    bendary - add  drag and object pooling

---
+++ Assets/_AppAssets/Scripts/DragableItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragableItem : MonoBehaviour
+{
+    private float distance;
+
+    private void Start()
+    {
+        distance = Camera.main.transform.position.z;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMousePos();
+    }
+
+    private void OnMouseUp()
+    {
+
+    }
+
+    private Vector2 GetMousePos()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        return mousePos;
+    }
+}
