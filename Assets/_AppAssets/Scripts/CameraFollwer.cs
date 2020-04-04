46ae7323773a88771aeff972de22b92428401ee0
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2020-04-04 11:04:28 +0200

    bendary - make chest and script item

---
+++ Assets/_AppAssets/Scripts/CameraFollwer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollwer : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        if (player)
+        {
+            offset = transform.position - player.position;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (player)
+        {
+            transform.position = player.position + offset;
+        }
+    }
+}
---
+++ Assets/_AppAssets/Scripts/ChestController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    [SerializeField] private GameObject chestTresure;
+    [SerializeField] private Sprite openedChest;
+    [SerializeField] private float disableDelay;
+
+    private Animator myAnim;
+
+    private void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    public void ToggleChest(bool enabled)
+    {
+        myAnim.SetBool("IsDissolve", enabled);
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Collider2D>().enabled = false;
+
+        GetComponent<SpriteRenderer>().sprite = openedChest;
+        GetComponent<SpriteRenderer>().material.SetTexture("_MainTextrue", openedChest.texture);
+
+        if (chestTresure)
+        {
+            GameObject go = Instantiate(chestTresure, transform);
+            go.transform.localPosition = Vector3.zero;
+            go.transform.SetParent(null);
+        }
+
+        StartCoroutine(DisableChest());
+    }
+
+    IEnumerator DisableChest()
+    {
+        yield return new WaitForSeconds(disableDelay);
+        ToggleChest(false);
+    }
+}
